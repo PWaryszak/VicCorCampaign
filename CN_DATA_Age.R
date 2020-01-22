67546d81238e42fae56ae397af92faabd8dbb33a
unknown <BlueCarbon@Bluecarbon-asus.du.deakin.edu.au>
2020-01-22 16:20:39 +1100

    Carbon and Nitrogen Accretion Rates computations and plots.

--- CN_DATA_Age.R
+++ CN_DATA_Age.R
@@ -2,7 +2,8 @@
 library(tidyverse)
 
 #VicCorCampaign Age Data:
-#Create dataset off Pere's file. MAR's units = g/cm2/y
+#Create MAR (Mass Accretion Rate) dataset
+#off Pere's spreadsheet. MAR's units = g/cm2/y
 MAR <- data.frame( site_habitat = c("Fren_Mangrove","Fren_Seagrass","Fren_Saltmarsh",
                                     "Jawb_Seagrass ","Jawb_Saltmarsh","Koow_Mangrove",
                                     "Rhyl_Mangrove","Rhyl_Seagrass","Rhyl_Saltmarsh",
@@ -22,16 +23,17 @@ cn2019$bay <- ifelse(cn2019$site == "Fren"|cn2019$site == "Rhyl"|cn2019$site == 
 
 cn2019 <- cn2019 %>%  unite( "site_habitat", c("site","habitat"), sep = "_", remove = F)
 
-
-#Join Pere's above data to Master data of cn2019:
+#CAR (Carbon Accretion Rate)======
+#Join  above MAR data to Master data of cn2019:
 cars <-  left_join(cn2019, MAR, by = "site_habitat") %>%
-  mutate(CAR_gcm2y = C.percent * MAR)%>%
+  mutate(CAR_gcm2y = C.percent/100 * MAR)%>% #Convert % into fraction by "/100"
   mutate (Stock = "Belowground")
 
-cars30<- filter(cars, DepthTo.cm <= 30 ) %>%  #till 30 cm deep because that is how deep the Age-dating was done to.
+cars30<- filter(cars, DepthTo.cm <= 30 ) %>%  #till 30 cm deep because that is
+  #how deep the Age-dating was done to.
   group_by(habitat) %>%
   summarise(AV = weighted.mean(CAR_gcm2y*100, na.rm = T), #1 g/cm2 = 100 tonnes per hectare
-            SD = sd(CAR_gcm2y*100, na.rm = T),
+            SD = sd(CAR_gcm2y*100, na.rm = T), 
             N  = n(), 
             SE = SD / sqrt(N))
 cars30
@@ -53,4 +55,73 @@ ggplot(cars30, aes(habitat, AV))  +
         plot.title = element_text(size = 20, face = "bold", vjust = 0.5),
         strip.background =  element_rect(fill = "white"))
 
-#bquote('Assimilation  ('*mu~ 'mol' ~t[2]~ cm^-2~y^-1*')')
+#bquote('Assimilation  ('*mu~ 'mol' ~t[2]~ cm^-2~y^-1*')') #nice function for special characters!
+
+ggsave(filename = "VIC_Age_600DPI.png", 
+       width = 8, 
+       height = 5,
+       dpi = 600)
+
+#CAR for Reiss, split by two bays======
+cars30<- filter(cars, DepthTo.cm <= 30 ) %>%  #till 30 cm deep because that is
+  #how deep the Age-dating was done to.
+  group_by(habitat, bay) %>%
+  summarise(AV = weighted.mean(CAR_gcm2y*100, na.rm = T), #1 g/cm2 = 100 tonnes per hectare
+            SD = sd(CAR_gcm2y*100, na.rm = T),
+            N  = n(), 
+            SE = SD / sqrt(N))
+
+ggplot(cars30, aes(habitat, AV))  +
+  geom_point(aes(color = habitat, size = 3)) +
+  geom_errorbar( aes(ymin= AV+SE, ymax = AV-SE), width=.2)+
+  labs(x= "", y = bquote('Carbon Accretion Rate  ' (Mg*~ha^-1 ~y^-1)))+
+  facet_grid(.~bay)+
+  #ggtitle("Carbon Accretion Rates in WPP & PPB habitats")+
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 16, color = "black"),
+        axis.text.y = element_text(size = 16, color = "black"),
+        axis.title.y = element_text(size = 16),
+        legend.position = "none",
+        legend.text = element_text(size = 16),
+        strip.text=element_text(size=16),
+        plot.title = element_text(size = 20, face = "bold", vjust = 0.5),
+        strip.background =  element_rect(fill = "white"))
+
+#Nitrogen=======
+#Join Pere's above data to Master data of cn2019 for Reiss:
+nars <-  left_join(cn2019, MAR, by = "site_habitat") %>%
+  mutate(NAR_gcm2y = N.percent/100 * MAR)%>% #Convert % into fraction by "/100"
+  mutate (Stock = "Belowground")
+
+nars30<- filter(nars, DepthTo.cm <= 30 ) %>%  #till 30 cm deep because that is
+  #how deep the Age-dating was done to.
+  group_by(habitat,bay) %>%
+  summarise(AV = weighted.mean(NAR_gcm2y*100, na.rm = T), #1 g/cm2 = 100 tonnes per hectare
+            SD = sd(NAR_gcm2y*100, na.rm = T), 
+            N  = n(), 
+            SE = SD / sqrt(N))
+nars30
+nars30$habitat <- factor(nars30$habitat, levels = c("Saltmarsh", "Mangrove","Seagrass"))
+
+ggplot(nars30, aes(habitat, AV))  +
+  geom_point(aes(color = habitat, size = 3)) +
+  geom_errorbar( aes(ymin= AV+SE, ymax = AV-SE), width=.2)+
+  labs(x= "", y = bquote('Nitrogen Accretion Rate  ' (Mg*~ha^-1 ~y^-1)))+
+  facet_grid(.~bay)+
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 16, color = "black"),
+        axis.text.y = element_text(size = 16, color = "black"),
+        axis.title.y = element_text(size = 16),
+        legend.position = "none",
+        legend.text = element_text(size = 16),
+        strip.text=element_text(size=16),
+        plot.title = element_text(size = 20, face = "bold", vjust = 0.5),
+        strip.background =  element_rect(fill = "white"))
+
+#bquote('Assimilation  ('*mu~ 'mol' ~t[2]~ cm^-2~y^-1*')') #nice function for special characters!
+
+ggsave(filename = "VIC_NAR_600DPI.png", 
+       width = 8, 
+       height = 5,
+       dpi = 600)
+
